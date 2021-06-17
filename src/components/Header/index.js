1ccfd0f5307bce2bf7a46a76ca324b7c24a8c1cb
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-06-17 11:44:30 -0700

    started designing header

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <header>
+            <h1 class="initials">SW</h1>
+            {/* Nav */}
+        </header>
+    );
+};
+
+export default Header;
