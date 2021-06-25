d4cb6236b30b2906b3e09b51522dec05e42896fa
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-06-24 21:03:12 -0700

    minor css adjustments

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Footer = () => {
 
     return (
-        <footer className="fixed-bottom container">
-            <ul className="row">
-                <li className='col'><a href="https://linkedin.com/in/sydneywalcoff">linkedin</a></li>
+        <footer className="fixed-bottom flex-row">
+            <ul className="row align-items-center">
+                <li className='col align-items-center'><a className="" href="https://linkedin.com/in/sydneywalcoff">linkedin</a></li>
                 <li className="col"><a  href="https://github.com/sydneywalcoff">github</a></li>
                 <li className="col"><a  href="https://stackoverflow.com/users/14700803/sydney-walcoff">stackOverflow</a></li>
             </ul>
