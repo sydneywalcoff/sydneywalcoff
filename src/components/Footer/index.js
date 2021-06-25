8ae464af183ea7eaf78607448876b2c5d2141119
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-06-25 10:29:47 -0700

    jk actually fixed ugliness

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
 
     return (
-        <footer className="fixed-bottom flex-row">
+        <footer className="fixed-bottom row">
             <ul className="row align-items-center">
                 <li className='col text-center'><a href="https://linkedin.com/in/sydneywalcoff">linkedin</a></li>
                 <li className="col text-center"><a href="https://github.com/sydneywalcoff">github</a></li>
