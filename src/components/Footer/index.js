269d8dd5db6f1d376b534b08ae5f49b07e742a9b
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-06-25 10:28:18 -0700

    fixed nav bar ugliness

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,9 +5,9 @@ const Footer = () => {
     return (
         <footer className="fixed-bottom flex-row">
             <ul className="row align-items-center">
-                <li className='col align-items-center'><a className="" href="https://linkedin.com/in/sydneywalcoff">linkedin</a></li>
-                <li className="col"><a  href="https://github.com/sydneywalcoff">github</a></li>
-                <li className="col"><a  href="https://stackoverflow.com/users/14700803/sydney-walcoff">stackOverflow</a></li>
+                <li className='col text-center'><a href="https://linkedin.com/in/sydneywalcoff">linkedin</a></li>
+                <li className="col text-center"><a href="https://github.com/sydneywalcoff">github</a></li>
+                <li className="col text-center"><a href="https://stackoverflow.com/users/14700803/sydney-walcoff">stackOverflow</a></li>
             </ul>
         </footer>
     );
