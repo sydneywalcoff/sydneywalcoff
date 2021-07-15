c50dcc52ab1b92299c822e09cd59b184ebcaf069
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-07-15 14:28:43 -0700

    updated resume,about section, and contact

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,8 +8,8 @@ const About = () => {
             <div className="row">
                 <img src={photo} alt="Sydney smiling in a grey turtleneck" className="col-sm-4" />
                 <div className="col align-self-center">
-                    <p >After graduating from UC Davis in 2017 with bachelor degrees in Music and in Economics, I'm currently on a journey to become a fullstack developer.</p>
-                    <p >When not learning a new programming language, I can be found making music, playing with my dog, Liba, or snowboarding.</p>
+                    <p >Full-Stack web developer brings strong teamwork skills to ensure success for any team. Recently completed full stack development course through UC Berkeley Extension, successfully developing skills like HTML, CSS, JavaScript, React.js, and Node.js. Known as a reliable, highly driven, and deeply determined problem solver. Always ready to lead or support my teammates to organize the project. Excited to bring passion and excitement to solving today’s most challenging problems.</p>
+                    <p >When not learning a new programming language, I can be found making music, or playing with my dog, Liba.</p>
                 </div>
             </div>
         </section>
