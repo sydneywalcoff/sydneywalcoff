2d1bc554d60787bf5bdb890602194ebb97d3083d
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-08-07 13:47:03 -0700

    moved react files into /client folder

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+
+    return (
+        <footer className="fixed-bottom row">
+            <ul className="row align-items-center">
+                <li className="col-xs-4"></li>
+                <li className='col text-center'><a rel="noreferrer" href="https://linkedin.com/in/sydneywalcoff" target="_blank">linkedin</a></li>
+                <li className="col text-center"><a rel="noreferrer" href="https://github.com/sydneywalcoff" target="_blank">github</a></li>
+                <li className="col text-center"><a rel="noreferrer" href="https://stackoverflow.com/users/14700803/sydney-walcoff" target="_blank">stackOverflow</a></li>
+                <li className="col-xs-4"></li>
+            </ul>
+        </footer>
+    );
+};
+
+export default Footer;
