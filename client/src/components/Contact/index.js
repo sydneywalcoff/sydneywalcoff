4eaee49a5c9baba68772a374529a3467d85aad6c
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-09-10 08:35:36 -0700

    center contact section

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ContactForm = () => {
    
     return (
-        <section className = "row contact p-3 ">
+        <section className = "contact p-3 ">
             <h1 className='row display-1 justify-content-center' id='title'>Contact me</h1>
             <div className="row">
                 <p className="text-center">reach out to me at <br></br><a href="mailto:sydney.walcoff@gmail.com">my email</a> <br></br>or<br></br> <a href="github.com/sydneywalcoff">my github</a> <br></br>or<br></br> <a href="linkedin.com/in/sydneywalcoff">my linkedin</a>
