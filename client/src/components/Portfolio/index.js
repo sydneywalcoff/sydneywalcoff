11055bb03c7ca0a6bd31afd38fb176d2690a4abc
Sydney Walcoff <sydney.walcoff@gmail.com>
2023-11-27 15:03:57 -0800

    reordered featured projects

--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -9,19 +9,19 @@ import appTrackPhoto from '../../assets/img/app-tracker.png';
 const Portfolio = () => {
     const featuredProjects = [
         {
+            name: 'Expunge Assist',
+            deployedUrl: 'https://expungeassist.org/',
+            repoUrl: 'https://github.com/hackforla/expunge-assist',
+            img: EAPhoto,
+            description: 'Expunge Assist is a TypeScript React letter generator tool to help justice-impacted individuals seek expungement.'
+        }, 
+        {
             name: 'AppTrack',
             deployedUrl: 'https://sw-app-tracker.herokuapp.com/',
             repoUrl: 'https://github.com/sydneywalcoff/app-tracker',
             img: appTrackPhoto,
             description: 'AppTrack is a TypeScript React MongoDB Node.js application for job seekers to track their applications and optimize their search.',
             etc: 'login: test | password: test'
-        },
-        {
-            name: 'Expunge Assist',
-            deployedUrl: 'https://expungeassist.org/',
-            repoUrl: 'https://github.com/hackforla/expunge-assist',
-            img: EAPhoto,
-            description: 'Expunge Assist is a TypeScript React letter generator tool to help justice-impacted individuals seek expungement.'
         }
     ];
     const projects = [
