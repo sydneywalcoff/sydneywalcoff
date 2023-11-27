da5029e0c82a4333ee418efb9816aafa6d1f0604
Sydney Walcoff <sydney.walcoff@gmail.com>
2023-11-27 15:21:36 -0800

    built freelance section, but left commented out for now

--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -14,17 +14,17 @@ const Portfolio = () => {
             repoUrl: 'https://github.com/hackforla/expunge-assist',
             img: EAPhoto,
             description: 'Expunge Assist is a TypeScript React letter generator tool to help justice-impacted individuals seek expungement.'
-        }, 
+        },
         {
             name: 'AppTrack',
             deployedUrl: 'https://sw-app-tracker.herokuapp.com/',
             repoUrl: 'https://github.com/sydneywalcoff/app-tracker',
             img: appTrackPhoto,
             description: 'AppTrack is a TypeScript React MongoDB Node.js application for job seekers to track their applications and optimize their search.',
-            etc: 'login: test | password: test'
+            etc: 'login & password: test'
         }
     ];
-    const projects = [
+    const bootcampProjects = [
         {
             name: 'MasterTicket2.0',
             repoUrl: 'https://github.com/UCB-Bootcamp/masterTicket2.0',
@@ -41,6 +41,15 @@ const Portfolio = () => {
             img: winePhoto
         },
     ];
+    // const freelanceProjects = [
+    //     {
+    //         name: 'Officiant of Love',
+    //         repoUrl: 'https://github.com/officiant-of-love/website',
+    //         deployedUrl: 'https://www.officiantoflovela.com/',
+    //         img: 'https://www.officiantoflovela.com/static/media/About.1d9abf3c74395c218dd5.jpg',
+    //         description: 'Helena is a Los Angeles based minister, ready to help you adjust to all phases of life.'
+    //     }
+    // ];
 
     return (
         <section className="portfolio container">
@@ -49,12 +58,18 @@ const Portfolio = () => {
             </div>
             <div className="projects row">
                 <div className='feature-project'>
-                    <Project project={featuredProjects[0]}/>
+                    <Project project={featuredProjects[0]} />
                     <Project project={featuredProjects[1]} />
                 </div>
-                <h3 className='display-6 text-center'>Bootcamp Projects</h3>
+                {/* <h3 className='display-6 text-center mt-3'>Freelance</h3>
+                <div className='secondary-projects'>
+                    {freelanceProjects.map(project => (
+                        <Project project={project} />
+                    ))}
+                </div> */}
+                <h3 className='display-6 text-center mt-3'>Bootcamp</h3>
                 <div className="tertiary-projects">
-                    {projects.map((project, i) => (
+                    {bootcampProjects.map(project => (
                         <Project project={project} />
                     ))}
                 </div>
