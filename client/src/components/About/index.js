ae89c4534a9691a128e3b670995dfade6e76ef2e
Sydney Walcoff <sydney.walcoff@gmail.com>
2023-11-26 20:25:04 -0800

    small about copy update

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -10,7 +10,7 @@ const About = () => {
                 <div className="col align-self-center about-text">
                     <p><b>techs:</b> HTML, CSS, JavaScript, TypeScript, PHP, React.js, and Node.js</p>
                     <p>I'm a Full-Stack Web Developer with Front-end expertise based in Los Angeles, CA. Currently volunteering with HackforLA as the Developer Lead on the <a target='_BLANK' rel='noreferrer' href="https://expungeassist.org">Expunge Assist</a> project.</p>
-                    <p>As a bootcamp graduate, I'm known as a reliable, highly driven, and deeply determined problem solver. I'm always ready to do what is necessary to support the project. I'm quick to pick up new technologies and languages on the fly.</p>
+                    <p>As a bootcamp graduate, I'm known as a reliable, highly driven, and deeply determined problem solver. I'm always ready to do what is necessary to support the project. I'm quick to pick up new technologies and languages on the fly and ready to find my next project.</p>
                     <p>Outside of coding, I spend my time watching baseball (Go Dodgers!), playing with my dog, Liba, snowboarding, and making music.</p>
                 </div>
             </div>
