aaa3a970c36cfcc8696a110a11c93937f0c82737
Sydney Walcoff <sydney.walcoff@gmail.com>
2023-11-27 15:04:49 -0800

    removed stackOverflow links

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,7 +8,6 @@ const Footer = () => {
                 <li className="col-xs-4"></li>
                 <li className='col text-center'><a rel="noreferrer" href="https://linkedin.com/in/sydneywalcoff" target="_blank">linkedin</a></li>
                 <li className="col text-center"><a rel="noreferrer" href="https://github.com/sydneywalcoff" target="_blank">github</a></li>
-                <li className="col text-center"><a rel="noreferrer" href="https://stackoverflow.com/users/14700803/sydney-walcoff" target="_blank">stackOverflow</a></li>
                 <li className="col-xs-4"></li>
             </ul>
         </footer>
