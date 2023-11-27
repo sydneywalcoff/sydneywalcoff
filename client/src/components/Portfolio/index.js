6eb02fe8a3bfe595dac8027528738f388d563e66
Sydney Walcoff <sydney.walcoff@gmail.com>
2023-11-26 17:37:08 -0800

    adding relevant projects and removing old projects

--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -3,54 +3,37 @@ import Project from '../Project';
 import winePhoto from '../../assets/img/wine.jpg';
 import musicPhoto from '../../assets/img/music.jpg';
 import musicPhoto2 from '../../assets/img/music2.jpg';
-import budgetPhoto from '../../assets/img/budget.jpg';
-import techBlogPhoto from '../../assets/img/tech-blog.jpg';
-import runPhoto from '../../assets/img/run.jpg';
-import pizzaPhoto from '../../assets/img/pizza.jpg';
 
 const Portfolio = () => {
-    const featuredProject =         {
-        name: 'MasterTicket2.0',
-        deployedUrl: 'https://masterticket2.herokuapp.com/',
-        repoUrl: 'https://github.com/UCB-Bootcamp/masterTicket2.0',
-        img: musicPhoto2
-    };
+    const featuredProjects = [
+        {
+            name: 'AppTrack',
+            deployedUrl: 'https://sw-app-tracker.herokuapp.com/',
+            repoUrl: 'https://github.com/sydneywalcoff/app-tracker',
+            img: musicPhoto2
+        },
+        {
+            name: 'Expunge Assist',
+            deployedUrl: 'https://expungeassist.org/',
+            repoUrl: 'https://github.com/hackforla/expunge-assist',
+            img: musicPhoto2
+        }
+    ];
     const projects = [
         {
+            name: 'MasterTicket2.0',
+            repoUrl: 'https://github.com/UCB-Bootcamp/masterTicket2.0',
+            img: musicPhoto2
+        },
+        {
             name: 'MasterTicket',
-            deployedUrl: 'https://masterticket.herokuapp.com/',
             repoUrl: 'https://github.com/UCB-Bootcamp/masterTicket',
             img: musicPhoto
         },
         {
             name: 'Wine and Dine',
-            deployedUrl: 'http://ucb-bootcamp.GitHub.io/wine-and-dine',
             repoUrl: 'https://github.com/UCB-Bootcamp/wine-and-dine',
             img: winePhoto
-        },
-        {
-            name: 'Budget Tracker',
-            deployedUrl: 'https://thawing-island-56218.herokuapp.com/',
-            repoUrl: 'https://github.com/sydneywalcoff/budget-tracker',
-            img: budgetPhoto
-        },
-        {
-            name: 'Tech Blog',
-            deployedUrl: 'https://stormy-dawn-50873.herokuapp.com/',
-            repoUrl: 'https://github.com/sydneywalcoff/tech-blog',
-            img: techBlogPhoto
-        },
-        {
-            name: 'Run Buddy',
-            deployedUrl: 'https://sydneywalcoff.github.io/run-buddy/',
-            repoUrl: 'https://github.com/sydneywalcoff/run-buddy',
-            img: runPhoto
-        },
-        {
-            name: 'Pizza Hunt ',
-            deployedUrl: 'https://boiling-depths-99742.herokuapp.com/',
-            repoUrl: 'https://github.com/sydneywalcoff/pizza-hunt',
-            img: pizzaPhoto
         },
     ];
 
@@ -60,13 +43,14 @@ const Portfolio = () => {
                 <h1 className="text-center display-3 title">Projects</h1>
             </div>
             <div className="projects row">
-                <div className ='feature-project'>
-                    < Project project={featuredProject} />
+                <div className='feature-project'>
+                    <Project project={featuredProjects[0]}/>
+                    <Project project={featuredProjects[1]} />
                 </div>
                 <div className="secondary-projects">
                     {projects.map((project, i) => (
-                        <div key={i} className="project m-2">
-                            < Project project={project} />
+                        <div key={i} className="m-2">
+                            <Project project={project} />
                         </div>
                     ))}
                 </div>
--- client/src/components/Project/index.js
+++ client/src/components/Project/index.js
@@ -3,16 +3,16 @@ import React from 'react';
 const Project = ({ project }) => {
     
     return (
-        <>
+        <div class="project">
             <img src={project.img} alt={project.description}/>
             <h3>{project.name}</h3>
             <div className="col align-self-center">
                 <div className="project-text row">
-                    <p className='col-6 text-center'><a href={project.deployedUrl}>live</a></p>
+                    {project.deployedUrl && (<p className='col-6 text-center'><a href={project.deployedUrl}>live</a></p>)}
                     <p className='col-6 text-center'><a href={project.repoUrl}>repo</a></p>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
